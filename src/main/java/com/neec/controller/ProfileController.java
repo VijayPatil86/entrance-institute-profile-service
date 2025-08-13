a8848ef84b859b521afef5e6b2b863590e0cd6a2
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-13 21:02:20 +0530

    initial commit

---
+++ src/main/java/com/neec/controller/ProfileController.java
@@ -0,0 +1,15 @@
+package com.neec.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/profiles")
+public class ProfileController {
+	@PostMapping("/me")
+	ResponseEntity<?> saveProfile(){
+		return null;
+	}
+}
