f077e5f648757709fee2c3ded7976f1ef6c94d2a
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-13 21:02:28 +0530

    initial commit

---
+++ src/main/java/com/neec/dto/CustomPrincipal.java
@@ -0,0 +1,15 @@
+package com.neec.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Builder @NoArgsConstructor @AllArgsConstructor @FieldDefaults(level = AccessLevel.PRIVATE) @Getter @Setter
+public class CustomPrincipal {
+	String subject;
+	String emailAddress;
+}
