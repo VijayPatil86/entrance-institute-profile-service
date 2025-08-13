45f05311325fc14ad9dd22544f63f97f4c5cd216
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-13 21:02:10 +0530

    initial commit

---
+++ src/main/java/com/neec/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.neec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.neec.filter.JwtAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+	static final private String[] PROFILE_ENDPOINTS = {
+			"/api/v1/profiles",
+			"/api/v1/profiles/me"
+	};
+
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return
+		httpSecurity.cors(Customizer.withDefaults())
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth.requestMatchers(PROFILE_ENDPOINTS).hasAuthority("APPLICANT"))
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.build();
+	}
+}
