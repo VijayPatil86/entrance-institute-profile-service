6b01cf8a622df4a0d95eef38f0ed9f6a162b7732
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-17 12:20:11 +0530

    added method to get profile for given user id

--- src/main/java/com/neec/service/ProfileServiceImpl.java
+++ src/main/java/com/neec/service/ProfileServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.neec.dto.ProfileRequestDTO;
+import com.neec.dto.ProfileResponseDTO;
 import com.neec.entity.StudentProfile;
 import com.neec.entity.UserLogin;
 import com.neec.repository.StudentProfileRepository;
@@ -49,5 +50,31 @@ public class ProfileServiceImpl implements ProfileService {
 				.percentage(dto.getPercentage())
 				.build();
 		StudentProfile savedStudentProfile = studentProfileRepository.save(studentProfile);
+	}
+
+	@Transactional(readOnly = true)
+	@Override
+	public ProfileResponseDTO getProfileByUserId(Long userId) {
+		StudentProfile studentProfile = studentProfileRepository.findById(userId)
+				.orElseThrow(() -> new IllegalArgumentException("Student profile with id " + userId + " does not exist."));
+		ProfileResponseDTO profileDTO = ProfileResponseDTO.builder()
+				.studentProfileId(studentProfile.getStudentProfileId())
+				.email(studentProfile.getUserLogin().getEmailAddress())
+				.firstName(studentProfile.getFirstName())
+				.lastName(studentProfile.getLastName())
+				.dateOfBirth(studentProfile.getDateOfBirth())
+				.gender(studentProfile.getGender())
+				.phoneNumber(studentProfile.getPhoneNumber())
+				.addressLine1(studentProfile.getAddressLine1())
+				.addressLine2(studentProfile.getAddressLine2())
+				.city(studentProfile.getCity())
+				.state(studentProfile.getState())
+				.pinCode(studentProfile.getPinCode())
+				.schoolName(studentProfile.getSchoolName())
+				.boardName(studentProfile.getBoardName())
+				.yearOfPassing(studentProfile.getYearOfPassing())
+				.percentage(studentProfile.getPercentage())
+				.build();
+		return profileDTO;
 	}
 }
