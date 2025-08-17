452f0dcdb2d5304997995f5d371e03d2a2feea4f
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-17 12:01:24 +0530

    added method to get profile for user id

--- src/main/java/com/neec/service/ProfileService.java
+++ src/main/java/com/neec/service/ProfileService.java
@@ -1,7 +1,10 @@
 package com.neec.service;
 
 import com.neec.dto.ProfileRequestDTO;
+import com.neec.dto.ProfileResponseDTO;
 
 public interface ProfileService {
 	void saveProfile(long userId, ProfileRequestDTO dto);
+
+	ProfileResponseDTO getProfileByUserId(Long userId);
 }
